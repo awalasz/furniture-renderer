6d0f06c3b787efaa7e65f4f06f79ca6d72821983
Artur Walasz <artur.walasz@revolve.pro>
2021-05-08 21:41:45 +0200

    feat: update shadowing tests

--- render_furniture/render_furniture/tests/test_remove_shadowed_geometries.py
+++ render_furniture/render_furniture/tests/test_remove_shadowed_geometries.py
@@ -7,23 +7,24 @@ Legend:
 xxx : closer geometry
 --- : further geometry
 
-Example:
-
-    xxxxxxxxx
-    x       x
-    x       x--+
-    x       x  |
-    xxxxxxxxx  |
-          |    |
-          +----+
 """
+import pytest
+
+from render_furniture.render_furniture.schemas import Geometry, PlaneChoices
+from render_furniture.render_furniture.utils import remove_shadowed_geometries
 
 CLOSE_X1 = 6
 CLOSE_X2 = 5
-FURTHER_X1 = 2
-FURTHER_X2 = 1
+FAR_X1 = 2
+FAR_X2 = 1
 
-def test_partially_shadowed():
+
+@pytest.fixture
+def plane():
+    return PlaneChoices.YZ
+
+
+def test_partially_shadowed(plane):
     """
     xxxxxxxxx
     x       x
@@ -33,19 +34,24 @@ def test_partially_shadowed():
           |    |
           +----+
     """
-    pass
+    close = Geometry(x1=CLOSE_X1, x2=CLOSE_X2, y1=0, y2=10, z1=0, z2=10)
+    far = Geometry(x1=FAR_X1, x2=FAR_X2, y1=5, y2=15, z1=5, z2=15)
+    out = remove_shadowed_geometries([far, close], plane)
+    assert out == [close, far]
 
 
 def test_not_overlapped():
     """
-
     xxxxxx    +----+
     x    x    |    |
     x    x    |    |
     xxxxxx    +----+
-
     """
-    pass
+    close = Geometry(x1=CLOSE_X1, x2=CLOSE_X2, y1=0, y2=10, z1=0, z2=5)
+    far = Geometry(x1=FAR_X1, x2=FAR_X2, y1=0, y2=10, z1=10, z2=15)
+    out = remove_shadowed_geometries([far, close], plane)
+    assert out == [close, far]
+
 
 
 def test_fully_shadowed():
@@ -55,9 +61,11 @@ def test_fully_shadowed():
     x  +--+ x
     x  +--+ x
     xxxxxxxxx
-
     """
-    pass
+    close = Geometry(x1=CLOSE_X1, x2=CLOSE_X2, y1=0, y2=15, z1=0, z2=15)
+    far = Geometry(x1=FAR_X1, x2=FAR_X2, y1=5, y2=10, z1=5, z2=10)
+    out = remove_shadowed_geometries([far, close], plane)
+    assert out == [close]
 
 
 def test_smaller_over_big():
@@ -69,4 +77,7 @@ def test_smaller_over_big():
     +--------+
 
     """
-    pass
+    close = Geometry(x1=CLOSE_X1, x2=CLOSE_X2, y1=5, y2=10, z1=5, z2=10)
+    far = Geometry(x1=FAR_X1, x2=FAR_X2, y1=0, y2=15, z1=0, z2=15)
+    out = remove_shadowed_geometries([far, close], plane)
+    assert out == [close, far]
